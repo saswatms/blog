65fd21f596d13730a057babf824458b749df378c
saswatms <saswatmishra.official@gmail.com>
2024-06-21 17:41:09 +0530

    Speed Insight Integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
         <Footer />
       </body>
+      <SpeedInsights />
     </html>
   );
 }
